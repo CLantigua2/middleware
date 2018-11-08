74e11ef495222228eaf71ccba0118684fd1b695d
Carlos Lantigua <clantigua954@gmail.com>
2018-11-08 13:49:36 -0500

    deleted yarn lock file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const server = require('./api/server.js');
-const port = 9000;
+// when pushing to a host process.env will give you a port, otherwise use local port
+const port = process.env.PORT || 9000;
 server.listen(port, () => console.log(`Server is over ${port}!!`));
